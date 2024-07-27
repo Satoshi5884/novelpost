dd1835e594a3b8b443a5d278b2b377ee482d7591
satoshi5884 <tsdb328@gmail.com>
2024-07-27 16:56:16 +0900

    Fix: Error02

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faFile, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faFile, faUser, faQuestion } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = ({ isAuth }) => {
   return (
@@ -14,8 +14,7 @@ const Navbar = ({ isAuth }) => {
           <div className="flex items-center">
             <Link to="/" className="px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:text-primary hover:bg-gray-50 group relative">
               <FontAwesomeIcon icon={faHouse} />
-              <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 
-              -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">Home</span>
+              <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">Home</span>
             </Link>
             {!isAuth ? (
               <Link to="/login" className="ml-4 px-3 py-2 rounded-md text-sm font-medium text-white bg-primary hover:bg-secondary">Login</Link>
@@ -23,13 +22,15 @@ const Navbar = ({ isAuth }) => {
               <>
                 <Link to="/createpost" className="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:text-primary hover:bg-gray-50 group relative">
                   <FontAwesomeIcon icon={faFile} />
-                  <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 
-                  -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">Create Post</span>
+                  <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">Create Post</span>
                 </Link>
                 <Link to="/mypage" className="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:text-primary hover:bg-gray-50 group relative">
                   <FontAwesomeIcon icon={faUser} />
-                  <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 
-                  -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">My Page</span>
+                  <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">My Page</span>
+                </Link>
+                <Link to="/help" className="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:text-primary hover:bg-gray-50 group relative">
+                  <FontAwesomeIcon icon={faQuestion} />
+                  <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">Help</span>
                 </Link>
                 <Link to="/logout" className="ml-4 px-3 py-2 rounded-md text-sm font-medium text-white bg-primary hover:bg-secondary">Logout</Link>
               </>
